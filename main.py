b027390ffbdb67b091b3532766a9a44b2f84d11f
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2021-03-10 23:24:09 +0000

    Update and rename scraper.py to main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from googlesearch import search
 import requests
 import re
 
+
 with open('proxy.txt', 'w') as f:
     for sites in search("free proxy server", tld="co.in", num=1000, stop=1000, pause=2): #pesquisa por"freeproxyserver" no google
         data = requests.get('{url}'.format(url=sites))
         proxys = re.findall(r'((?:\d{1,3}\.){3}\d{1,3}):(\d+)', data.text)
         for proxy in proxys: 
-            f.write(str(proxys))
+            f.write(str(proxy)) #guarda no file "proxy.txt"
