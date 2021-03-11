7cb9b33445f8ab51b92797b50694ab4aa311e233
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2021-03-11 23:02:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from bs4 import BeautifulSoup
 from googlesearch import search
 import requests
-import re
 import subprocess
+import numpy as np
+import re
 
-#Remove os seguintes caracteres: ()',
-def cleanProxy(proxy):
-    proxy = str( re.sub('[()\' ]','',str(proxy)) )
-    proxy = str( re.sub(',',':',str(proxy)) )
-    return proxy
 
 
 
 with open('proxy.txt', 'w') as f:
+    
     for website in search("free proxy server", tld="co.in", num=1000, stop=1000, pause=2): #pesquisa por"freeproxyserver" no google
-        print("Searching in: "+website)
-        data = requests.get('{url}'.format(url=website))
-        proxys = re.findall(r'((?:\d{1,3}\.){3}\d{1,3}):(\d+)', data.text)
+        print("Searching in: "+website) 
+        data = requests.get('{url}'.format(url=website))                     
+        proxys = re.findall(r'((?:\d{1,3}\.){3}\d{1,3}[:]\d+)', data.text)      #procura por proxies no site
         for proxy in proxys: 
-            f.write(cleanProxy(proxy)+"\n") #guarda no file "proxy.txt"
-            print("Proxy Found: "+cleanProxy(proxy))
-            print("Ping to: "+cleanProxy(proxy)+" whit status: "+str( subprocess.call('ping '+cleanProxy(proxy)) ))
+            
+            f.write(proxy+"\n")                             #guarda no file "proxy.txt"
+            print("Proxy Found: "+proxy)
+  
+            clean = proxy.split(':')[0]                      #ip sem a porta
+            print("Ping to: "+proxy+" whit status: "+str( subprocess.call('ping '+cleanProxy(proxy)) ))
     
 
